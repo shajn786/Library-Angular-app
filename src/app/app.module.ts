e28925d6b2bff18a7b9bdb514945f0adb276df79
shajn786 <shajn786@gmail.com>
2022-11-24 21:50:09 +0530

    view all books

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,14 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { AdminLoginComponent } from './admin-login/admin-login.component';
 import { BookEntryComponent } from './book-entry/book-entry.component';
+import { BookViewAllComponent } from './book-view-all/book-view-all.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     AdminLoginComponent,
-    BookEntryComponent
+    BookEntryComponent,
+    BookViewAllComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/book-view-all/book-view-all.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-book-view-all',
+  templateUrl: './book-view-all.component.html',
+  styleUrls: ['./book-view-all.component.css']
+})
+export class BookViewAllComponent {
+
+}
