2d2960b7ac5aa067cbcacf4f8f3aa6682086a8f7
shajn786 <shajn786@gmail.com>
2022-11-25 11:21:20 +0530

    json array and databinding

--- src/app/book-view-all/book-view-all.component.ts
+++ src/app/book-view-all/book-view-all.component.ts
@@ -7,4 +7,58 @@ import { Component } from '@angular/core';
 })
 export class BookViewAllComponent {
 
+  book:any =[
+    {
+      "title":"Malorie",
+      "image":"https://i.pinimg.com/474x/b1/41/c4/b141c4ad928f1802711886e24fe26f01.jpg",
+      "author":"Josh Malerman "
+    },
+    {
+      "title":"Heart Shaped Box",
+      "image":"https://m.media-amazon.com/images/I/71Eex9n-rCL.jpg",
+      "author":"Joe Hill"
+    },
+    {
+      "title":"Bird Box",
+      "image":"https://m.media-amazon.com/images/I/51G9z1ZPAvL.jpg",
+      "author":"Josh Malerman"
+    },
+    {
+      "title":"The Vesuvius Club",
+      "image":"https://m.media-amazon.com/images/I/819DG6hgc1L.jpg",
+      "author":"marl glais"
+    },
+    {
+      "title":"The Vesuvius Club",
+      "image":"https://kbimages1-a.akamaihd.net/b8962b07-61f8-409b-93de-45dd06cf920c/1200/1200/False/heart-shaped-box.jpg",
+      "author":"marl glais"
+    },
+    {
+      "title":"Heart Shaped Box",
+      "image":"https://kbimages1-a.akamaihd.net/b8962b07-61f8-409b-93de-45dd06cf920c/1200/1200/False/heart-shaped-box.jpg",
+      "author":"Josh Malerman"
+    },
+    {
+      "title":"The Girl in the Box",
+      "image":"https://i.gr-assets.com/images/S/compressed.photo.goodreads.com/books/1444019284l/283142.jpg",
+      "author":"marl glais"
+    },
+    {
+      "title":"The Sacred Ruins",
+      "image":"https://cdn.novelupdates.com/images/2017/03/The-Sacred-Ruins.jpg",
+      "author":"marl glais"
+    },
+    {
+      "title":"I can See The Line",
+      "image":"https://cdn.novelupdates.com/images/2021/01/I-Can-See-the-Lines-of-Fortune.jpg",
+      "author":"Gien Dong"
+    },
+    {
+      "title":"I Love You Monster",
+      "image":"https://i.gr-assets.com/images/S/compressed.photo.goodreads.com/books/1568096049l/53031498._SX1200_SY630_.jpg",
+      "author":"Kazzenlx"
+    }
+    
+  ]
+
 }
