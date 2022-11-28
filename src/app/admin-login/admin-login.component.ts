4617ea13e84ac24b35d5a00e8d907796eb139ca2
shajn786 <shajn786@gmail.com>
2022-11-28 15:06:44 +0530

    reading values

--- src/app/admin-login/admin-login.component.ts
+++ src/app/admin-login/admin-login.component.ts
@@ -7,4 +7,18 @@ import { Component } from '@angular/core';
 })
 export class AdminLoginComponent {
 
+  username=""
+  password=""
+
+  readValues = ()=>
+  {
+     let data:any = {
+      "username":this.username,
+      "password":this.password
+     }
+
+     console.log(data)
+
+  }
+
 }
